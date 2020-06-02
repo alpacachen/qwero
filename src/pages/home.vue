87d295ac719ef03d141bd6fb5224c1949a8ff79d
chenxiang <chenxiang2015@ipa;fish.com>
2020-06-02 09:59:25 +0800

    animate4phaser

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -29,7 +29,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   background: #000000;
   height: 100vh;
   overflow: hidden;
