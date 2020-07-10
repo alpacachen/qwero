aa73e560ee90238fa3536a7b2650e8cbef7285dc
chenxiang <chenxiang2015@ipa;fish.com>
2020-07-10 16:15:11 +0800

    更新缓存

--- oss.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    region:'oss-cn-beijing',
-    AccessKey: 'LTAI4GAYtpZN86e3h1vaDCyC',
-    SECRET: 'qNdK723VsJB6x2ouG2gOKseUS8yCrm',
-    UserName: 'qwero@1500898344315634.onaliyun.com',
-    bucket: 'cx-test-index',
-}
