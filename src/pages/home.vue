0a770f1a966dd6df3cd237600ed559bcebdb215d
陈湘 <chenxiangbj@fenbi.com>
2021-03-20 15:34:01 +0800

    ADD: 增加可达鸭外链

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,7 +3,7 @@
     <div id="svg"></div>
     <div style="overflow: hidden;">
       <p>
-        <router-link to="http://www.qwero.cn/react/index.html">可达鸭</router-link>
+        <a href="http://www.qwero.cn/react/index.html" target="_blank">可达鸭</a>
       </p>
       <p>
         <router-link to="/animate4phaser">animate4phaser</router-link>
