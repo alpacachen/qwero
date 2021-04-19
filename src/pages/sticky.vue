f15edecea7bba08a92509e2a20dbd75bbe355648
陈湘 <chenxiangbj@fenbi.com>
2021-04-19 17:30:56 +0800

    FIX: sticky event

--- src/pages/sticky.vue
+++ src/pages/sticky.vue
@@ -20,44 +20,65 @@
       </div>
       <div ref="container2" class="container">
         <div class="m-2">
+          <div class="content"></div>
+          <div class="content"></div>
           <p class="sticky">标题1</p>
           <div class="content"></div>
           <div class="content"></div>
         </div>
         <div class="m-2">
+          <div class="content"></div>
+          <div class="content"></div>
           <p class="sticky">标题2</p>
           <div class="content"></div>
           <div class="content"></div>
         </div>
         <div class="m-2">
+          <div class="content"></div>
+          <div class="content"></div>
           <p class="sticky">标题3</p>
           <div class="content"></div>
           <div class="content"></div>
         </div>
         <div class="m-2">
+          <div class="content"></div>
+          <div class="content"></div>
           <p class="sticky">标题4</p>
           <div class="content"></div>
           <div class="content"></div>
         </div>
       </div>
     </el-card>
+    <el-card class="card" shadow="hover">
+      <div slot="header">
+        <span>例子3</span>
+      </div>
+      <div ref="container3" class="container">
+        <div class="content"></div>
+        <div class="content"></div>
+        <p style="bottom: 10px" class="sticky">bottom</p>
+        <div class="content"></div>
+        <div class="content"></div>
+        <div class="content"></div>
+        <div class="content"></div>
+      </div>
+    </el-card>
   </div>
 </template>
 <script>
-import { watchSticky } from "../utils/stickyEvent";
+import { watchSticky } from "@/utils/stickyEvent";
 export default {
   mounted() {
-    document.addEventListener("sticky-change", function (e) {
-      const { detail } = e;
-      const { target, status } = detail;
-
+    const cb = function (target, status) {
       target.classList.toggle("shadow", status);
-    });
-    watchSticky(this.$refs["container1"].querySelector(".sticky"));
+    }
+
+    watchSticky(this.$refs["container1"].querySelector(".sticky")).on('sticky-change', cb);
+    watchSticky(this.$refs["container3"].querySelector(".sticky")).on('sticky-change', cb);
 
     Array.from(
       this.$refs["container2"].querySelectorAll(".sticky")
-    ).forEach((dom) => watchSticky(dom, this.$refs["container2"]));
+    ).forEach((dom) => watchSticky(dom, this.$refs["container2"]).on('sticky-change', cb));
   },
 };
 </script>
@@ -75,7 +96,8 @@ export default {
   position: sticky;
   margin: 0;
   padding: 0;
-  top: 40px;
+  top: 10px;
+  bottom: 10px;
 }
 .card {
   max-width: 375px;
--- src/utils/stickyEvent.js
+++ src/utils/stickyEvent.js
@@ -1,47 +1,88 @@
-
+import EventEmitter from 'eventemitter3'
 export function watchSticky(element, container) {
-    element.style.cssText = `position: -webkit-sticky;position: sticky;`;
-    const stickyValue = window.getComputedStyle(element).getPropertyValue('top')
+    const ee = new EventEmitter()
+    element.style.cssText += `position: -webkit-sticky;position: sticky;`;
+    const stickyValueTop = window.getComputedStyle(element).getPropertyValue('top')
+    const stickyValueBottom = window.getComputedStyle(element).getPropertyValue('bottom')
     const elementHeight = element.getBoundingClientRect().height
     const parentElement = element.parentElement;
+    if(stickyValueTop !== 'auto') {
+        // 在顶部增加元素
+        const top = document.createElement('div');
+        top.style.cssText = `position: relative;top: -${stickyValueTop};`
+        top.classList.add('_sticky_top');
+        parentElement.insertBefore(top, element);
 
-    // 在顶部增加元素
-    const top = document.createElement('div');
-    top.style.cssText = `position: relative;top: -${stickyValue};`
-    top.classList.add('_sticky_top');
-    parentElement.insertBefore(top, element);
+        // 在底部增加元素
+        const bottom = document.createElement('div');
+        bottom.style.cssText = `position: relative;top: -${parseFloat(stickyValueTop) + elementHeight}px;`
+        bottom.classList.add('_sticky_bottom');
+        parentElement.appendChild(bottom)
 
-    // 在底部增加元素
-    const bottom = document.createElement('div');
-    bottom.style.cssText = `position: relative;top: -${parseFloat(stickyValue) + elementHeight}px;`
-    bottom.classList.add('_sticky_bottom');
-    parentElement.appendChild(bottom)
-
-    function topCb([info]) {
-        const rootInfo = info.rootBounds
-        const targetInfo = info.boundingClientRect
-        if (targetInfo.top < rootInfo.bottom && info.isIntersecting) {
-            fire(element, false)
-        } else if (targetInfo.top <= rootInfo.top && !info.isIntersecting) {
-            fire(element, true)
+        const topCb = ([info]) => {
+            const rootInfo = info.rootBounds
+            const targetInfo = info.boundingClientRect
+            if (targetInfo.top < rootInfo.bottom && info.isIntersecting) {
+                fire(element, false)
+            }
+            if (targetInfo.top <= rootInfo.top && !info.isIntersecting) {
+                fire(element, true)
+            }
         }
-    }
-    const topOb = new IntersectionObserver(topCb, { root: container || parentElement, threshold: 0 })
-    topOb.observe(top)
+        const topOb = new IntersectionObserver(topCb, { root: container || parentElement, threshold: 0 })
+        topOb.observe(top)
 
-    function bottomCb([info]) {
-        const rootInfo = info.rootBounds
-        const targetInfo = info.boundingClientRect
-        if (targetInfo.top < rootInfo.bottom && info.isIntersecting) {
-            fire(element, true)
-        } else if (targetInfo.top <= rootInfo.top && !info.isIntersecting) {
-            fire(element, false)
+        const bottomCb = ([info]) => {
+            const rootInfo = info.rootBounds
+            const targetInfo = info.boundingClientRect
+            if (targetInfo.top < rootInfo.bottom && info.isIntersecting) {
+                fire(element, true)
+            } else if (targetInfo.top <= rootInfo.top && !info.isIntersecting) {
+                fire(element, false)
+            }
         }
+        const bottomOb = new IntersectionObserver(bottomCb, { root: container || parentElement, threshold: 0 })
+        bottomOb.observe(bottom)
     }
-    const bottomOb = new IntersectionObserver(bottomCb, { root: container || parentElement, threshold: 0 })
-    bottomOb.observe(bottom)
-}
+    if(stickyValueBottom !== 'auto') {
+        const bottom = document.createElement('div');
+        bottom.style.cssText = `position: relative;top: ${parseFloat(stickyValueBottom) + elementHeight}px`
+        bottom.classList.add('_sticky_bottom');
+        parentElement.insertBefore(bottom, element);
+        const b_bottomCb = ([info]) => {
+            const rootInfo = info.rootBounds
+            const targetInfo = info.boundingClientRect
+            if(targetInfo.top < rootInfo.bottom && info.isIntersecting) {
+                fire(element, false)
+            } else if(targetInfo.top >= rootInfo.top && !info.isIntersecting){
+                fire(element, true)
+            }
+        }
+        const bottomOb = new IntersectionObserver(b_bottomCb, { root: container || parentElement, threshold: 0 })
+        bottomOb.observe(bottom)
 
-function fire(target, status) {
-    document.dispatchEvent(new CustomEvent('sticky-change', { detail: { target, status } }))
+        // 在顶部增加元素
+        const top = document.createElement('div');
+        top.style.cssText = `position: relative;top: ${parseFloat(stickyValueBottom) + elementHeight}px`
+        top.classList.add('_sticky_top');
+        parentElement.prepend(top);
+
+        // eslint-disable-next-line no-unused-vars
+        const b_topCb = ([info]) => {
+            const rootInfo = info.rootBounds
+            const targetInfo = info.boundingClientRect
+            if (targetInfo.top > rootInfo.top && info.isIntersecting) {
+                fire(element, true)
+            }
+            if (targetInfo.top >= rootInfo.bottom && !info.isIntersecting) {
+                fire(element, false)
+            }
+        }
+        const topOb = new IntersectionObserver(b_topCb, { root: container || parentElement, threshold: 0 })
+        topOb.observe(top)
+    }
+    function fire(target, status) {
+        ee.emit('sticky-change', target, status)
+    }
+    return ee
 }
